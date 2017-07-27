7f37e2bc863fbc1dba55922b970db81bf717b7a9
Jili Li <jilili44@gmail.com>
2017-07-27 17:54:37 -0300

    separated app content from the main index.js to the app.js in the components folder

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,19 @@
-/*
-This app followed the course -- Modern React with Redux from udemy
-Some comment line start with, e.g. S1T13, S1T13 means the following content is from section 1 Tutorial 13
-from the course 
-*/
 
+import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import VideoDetail from './video_detail';
+import VideoList from './video_list';
+//S2T32: after installing lodash using npm (npm install --save lodash), import it here
+//lodash can adjust the frequency the search keywords user types in in the search bar
+import _ from 'lodash';
 
-import React, { Component } from 'react'; //create a instance of a certain class
-import ReactDom from 'react-dom'; //render a component to display on a webpage
+//S2T21 import the youtube api search
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
+//S1T13: to use google youtube API, the first step is to get an API key from google
+const API_KEY = 'AIzaSyAmoz4DqY5lgezXqHWI1hSAPukln5yE3-I';
 
-//step1: create a new component. This component should produce some HTML
-//Note belowing App is actually a class, not an instance!
-
-/*
-//S2T22 refactor the App function into a class
-//use a class component if the component needs to have the state
-class App extends Component {           //()=>{function body} is the same as function(){function body} except the meaning of 'this'
+export default class App extends Component{
   constructor(props){
     super(props);
 
@@ -40,15 +37,12 @@ class App extends Component {           //()=>{function body} is the same as fun
       });
     });
   }
-  
+
   render(){
     //S2T32: use lodash's debounce method to throttle the videoSearch function to be called every 300 mss
     const videoSearchThrottled = _.debounce((term) => {this.videoSearch(term)}, 300)
-    
-    
-    
-    
-    return (
+
+    return(
       //create a SearchBar instance and put it in div
       <div>
         <SearchBar 
@@ -70,34 +64,6 @@ class App extends Component {           //()=>{function body} is the same as fun
                                               //the 'props' object is passed down to the child by calling super(props) in the child's constructor
         />
       </div>
-    );       
+    );
   }
 }
-
-/*
-//S1T14
-const App = () => {           //()=>{function body} is the same as function(){function body} except the meaning of 'this'
-  return (
-    //create a SearchBar instance and put it in div
-    <div>
-      <SearchBar />  
-    </div>);
-}
-
-//before S1T14
-const App = () => {           //()=>{function body} is the same as function(){function body} except the meaning of 'this'
-  return <div>Hi!</div>;
-}
-
-
-const App = function(){
-  return <div>Hi!</div>;
-}
-*/
-
-//step2: Take this conponent's generated HTML and put it on the page (in the DOM)
-
-
-//<App /> is a JSX syntax to create an instance of App class
-//second argument of render() is to indicate where to render the component to
-ReactDom.render(<App />, document.querySelector('.container')); 
